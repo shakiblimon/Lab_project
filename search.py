703d56e6c10dfc0542bb2f955efdde5b8436d6a6
shakiblimon <shakibcselimon@gmail.com>
2017-04-15 00:30:49 +0600

    Initial commit

---
+++ search.py
@@ -0,0 +1,13 @@
+# searching string for matching with expression
+import re
+
+line = "Cats are smarter than dogs";
+
+searchObj = re.search( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if searchObj:
+   print ("searchObj.group() : ", searchObj.group())
+   print ("searchObj.group(1) : ", searchObj.group(1))
+   print ("searchObj.group(2) : ", searchObj.group(2))
+else:
+   print ("Nothing found!!")
